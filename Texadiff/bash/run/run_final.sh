c2134d8242c42c30d9385d28651b0da5bfd0baca
Zez <108481677+ZezFuture@users.noreply.github.com>
2026-05-06 14:16:49 +0800

    Update run_final.sh

--- Texadiff/bash/run/run_final.sh
+++ Texadiff/bash/run/run_final.sh
@@ -1,14 +1,14 @@
 
 
 python3 run.py --pretrained_model_name_or_path "checkpoint/stable-diffusion-xl-base-1.0" \
-    --unet_model_name_or_path "/home/nju/zez/ADRSSR/train_ckpt/1107v_final/checkpoints/checkpoint-30000" \
-    --controlnet_model_name_or_path "/home/nju/zez/ADRSSR/train_ckpt/1107v_final/checkpoints/checkpoint-30000" \
+    --unet_model_name_or_path "" \
+    --controlnet_model_name_or_path "" \
     --controlnet_scale 1.0 \
     --vae_model_name_or_path "checkpoint/sdxl-vae-fp16-fix" \
     --validation_prompt "high-resolution. 8k, clean" \
     --negative_prompt "blurry, dotted, noise, raster lines, unclear, low-resolution, over-smoothed" \
-    --validation_image "/home/nju/zez/ADRSSR/testdata/SIRI-WHU/12class_png" \
-    --output_dir "infer_cvpr/1107final_siri_maskmodelv4_thr0.35_maxpool_in100to500_min2000_withfix" \
+    --validation_image "" \
+    --output_dir "" \
     --variant fp16 \
     --num_inference_steps 20 \
     --use_safetensors \
